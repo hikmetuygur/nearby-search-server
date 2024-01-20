347e8f0613856aa9e17c74bc4fae20b445460382
Hikmet <uygurhikmet@gmail.com>
2024-01-20 11:35:42 +0300

    Initial Commit

---
+++ nearby-search/src/main/java/com/huygur/nearbysearch/NearbySearchApplication.java
@@ -0,0 +1,13 @@
+package com.huygur.nearbysearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NearbySearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NearbySearchApplication.class, args);
+	}
+
+}
---
+++ nearby-search/src/test/java/com/huygur/nearbysearch/NearbySearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huygur.nearbysearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NearbySearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
